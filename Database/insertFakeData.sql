6a4ebadab213a67a6e959ae09d6d35ca3f224b5c
Biagio Festa <biagio.festa@gmail.com>
2017-06-09 11:20:08 +0200

    Different queries

--- Database/insertFakeData.sql
+++ Database/insertFakeData.sql
@@ -11,7 +11,9 @@ INSERT INTO `APPLICATION_PROFILE_TABLE` (
   `phi_core`,
   `vir_core`,
   `chi_0`,
-  `chi_c`) VALUES ('query44', 123, 123, 123, 123, 123, 123, 123);
+  `chi_c`) VALUES ('query44', 123, 123, 123, 123, 123, 123, 123),
+                  ('query45', 123, 123, 123, 123, 123, 123, 123)
+                  ('query46', 123, 123, 123, 123, 123, 123, 123);
 
 INSERT INTO `RUNNING_APPLICATION_TABLE` (
   `application_session_id`,
@@ -21,5 +23,5 @@ INSERT INTO `RUNNING_APPLICATION_TABLE` (
   `status`,
   `ending_time`)
     VALUES ('application_1483347394756_0', 'query44', 123, CURRENT_TIMESTAMP, 'RUNNING', 0),
-           ('application_1483347394756_1', 'query44', 123, CURRENT_TIMESTAMP, 'RUNNING', 0),
-           ('application_1483347394756_2', 'query44', 123, CURRENT_TIMESTAMP, 'RUNNING', 0);
+           ('application_1483347394756_1', 'query45', 123, CURRENT_TIMESTAMP, 'RUNNING', 0),
+           ('application_1483347394756_2', 'query46', 123, CURRENT_TIMESTAMP, 'RUNNING', 0);
