8fc7a78274c7c41405e6686b13c53323ef95ba76
Biagio Festa <biagio.festa@gmail.com>
2017-06-14 14:23:21 +0200

    Different fake data for database

--- Database/insertFakeData.sql
+++ Database/insertFakeData.sql
@@ -11,9 +11,8 @@ INSERT INTO `APPLICATION_PROFILE_TABLE` (
   `phi_core`,
   `vir_core`,
   `chi_0`,
-  `chi_c`) VALUES ('query44', 123, 123, 123, 123, 123, 123, 123),
-                  ('query45', 123, 123, 123, 123, 123, 123, 123),
-                  ('query46', 123, 123, 123, 123, 123, 123, 123);
+  `chi_c`) VALUES ('Q26', 123, 123, 123, 123, 123, 123, 123),
+                  ('Q52', 123, 123, 123, 123, 123, 123, 123);
 
 INSERT INTO `RUNNING_APPLICATION_TABLE` (
   `application_session_id`,
@@ -22,6 +21,5 @@ INSERT INTO `RUNNING_APPLICATION_TABLE` (
   `submission_time`,
   `status`,
   `ending_time`)
-    VALUES ('application_1483347394756_0', 'query44', 123, CURRENT_TIMESTAMP, 'RUNNING', 0),
-           ('application_1483347394756_1', 'query45', 123, CURRENT_TIMESTAMP, 'RUNNING', 0),
-           ('application_1483347394756_2', 'query46', 123, CURRENT_TIMESTAMP, 'RUNNING', 0);
+    VALUES ('application_1483347394756_0', 'Q26', 123, CURRENT_TIMESTAMP, 'RUNNING', 0),
+           ('application_1483347394756_1', 'Q52', 123, CURRENT_TIMESTAMP, 'RUNNING', 0);
