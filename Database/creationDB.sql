95086aa3ce623bda868def7f4573a97cbc197bd2
Biagio Festa <biagio.festa@gmail.com>
2017-09-27 12:24:57 +0200

    Updated creation DB script and Dockerfile with new OPT_IC and OPT_Deadline

--- Database/creationDB.sql
+++ Database/creationDB.sql
@@ -9,6 +9,7 @@ DROP TABLE IF EXISTS `OPTIMIZER_CONFIGURATION_TABLE`;
 DROP TABLE IF EXISTS `RUNNING_APPLICATION_TABLE`;
 DROP TABLE IF EXISTS `APPLICATION_PROFILE_TABLE`;
 DROP TABLE IF EXISTS `OPT_SESSIONS_RESULTS`;
+DROP TABLE IF EXISTS `PREDICTOR_CACHE_TABLE`;
 
 -- Allow zero timestamp
 SET sql_mode = '';
@@ -22,7 +23,7 @@ CREATE TABLE `APPLICATION_PROFILE_TABLE` (
   `vir_core` INT NOT NULL,
   `chi_0` DOUBLE NOT NULL,
   `chi_c` DOUBLE NOT NULL,
-  PRIMARY KEY (`application_id`)
+  PRIMARY KEY (`application_id`, `dataset_size`)
 );
 
 CREATE TABLE `RUNNING_APPLICATION_TABLE` (
@@ -32,8 +33,10 @@ CREATE TABLE `RUNNING_APPLICATION_TABLE` (
   `submission_time` TIMESTAMP NOT NULL,
   `status` VARCHAR(100) NOT NULL,
   `ending_time` TIMESTAMP DEFAULT 0,
+  `weight` DOUBLE NOT NULL,
+  `deadline` DOUBLE NOT NULL,
   PRIMARY KEY (`application_session_id`),
-  FOREIGN KEY (`application_id`) REFERENCES `APPLICATION_PROFILE_TABLE`(`application_id`)
+  FOREIGN KEY (`application_id`, `dataset_size`) REFERENCES `APPLICATION_PROFILE_TABLE`(`application_id`, `dataset_size`)
 );
        
 CREATE TABLE `OPTIMIZER_CONFIGURATION_TABLE` (
@@ -43,13 +46,24 @@ CREATE TABLE `OPTIMIZER_CONFIGURATION_TABLE` (
   `num_cores_opt` INT NOT NULL,
   `num_vm_opt` INT NOT NULL,
   PRIMARY KEY (`application_id`,`dataset_size`, `deadline`),
-  FOREIGN KEY (`application_id`) REFERENCES `APPLICATION_PROFILE_TABLE`(`application_id`)
+  FOREIGN KEY (`application_id`, `dataset_size`) REFERENCES `APPLICATION_PROFILE_TABLE`(`application_id`, `dataset_size`)
 );
 
-CREATE TABLE `OPT_SESSIONS_RESULTS` (
+CREATE TABLE `OPT_SESSIONS_RESULTS_TABLE` (
    `opt_id` VARCHAR(255) NOT NULL,
    `app_id` VARCHAR(100) NOT NULL,
-   `nu` FLOAT,
+   `num_cores` INT,
+   `num_vm` INT,
    PRIMARY KEY(`opt_id`, `app_id`)
+);
+
+CREATE TABLE `PREDICTOR_CACHE_TABLE` (
+   `application_id` VARCHAR(100) NOT NULL,
+   `dataset_size` DOUBLE NOT NULL,
+   `n_cores` INT NOT NULL,
+   `stage` VARCHAR(10) NOT NULL,
+   `val` DOUBLE NOT NULL,
+   PRIMARY KEY(`application_id`, `dataset_size`, `n_cores`, `stage`),
+   FOREIGN KEY (`application_id`, `dataset_size`) REFERENCES `APPLICATION_PROFILE_TABLE`(`application_id`, `dataset_size`) 
 );
 
