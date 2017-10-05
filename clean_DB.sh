6905d4c9edf165809ed5f7d7e1f6c5891d3db8bf
Marco Lattuada <marco.lattuada@polimi.it>
2017-10-05 12:10:06 +0200

    Added script to clean db

---
+++ clean_DB.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Copyright 2017 <Marco Lattuada>
+
+abs_script=`readlink -e $0`
+dir_script=`dirname $abs_script`
+
+DOCKER_DB_CONTAINER_NAME=mysql_bigsea
+
+SCRIPT_FILE=/clean_DB.sql
+MYSQL_USER=bigsea
+DOCKER_WS_CONTAINER_NAME=wsi_service
+DOCKER_MYSQL_PORT=3306
+MYSQL_DATABASE=bigsea
+
+grep_command="grep OptDB_pass wsi_config.xml"
+password_line=`docker exec ${DOCKER_WS_CONTAINER_NAME} /bin/bash -c "${grep_command}"`
+user_db_password=`echo $password_line | awk -F\> {'print $2'} | awk -F\< {'print $1'}`
+
+#Inserting data for OPT_JR
+docker cp ${dir_script}/Database/clean_DB.sql ${DOCKER_DB_CONTAINER_NAME}:${SCRIPT_FILE}
+docker exec ${DOCKER_DB_CONTAINER_NAME} bin/bash -c "mysql -u${MYSQL_USER} -p${user_db_password} ${MYSQL_DATABASE} < ${SCRIPT_FILE}";
+echo "Done"
