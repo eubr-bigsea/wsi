093ac000de2ce43ca4b3be7331093bae517d1445
Marco Lattuada <marco.lattuada@polimi.it>
2017-10-17 15:59:21 +0200

    Updated submission time to fixed values in fake data

--- Database/insertFakeData.sql
+++ Database/insertFakeData.sql
@@ -24,10 +24,10 @@ INSERT INTO `RUNNING_APPLICATION_TABLE` (
   `weight`,
   `deadline`,
   `num_cores`)
-    VALUES ('application_1483347394756_0', 'query26', 1000, CURRENT_TIMESTAMP, 'RUNNING', 1, 200000, 4),
-                   ('application_1483347394756_1', 'query52', 1000, CURRENT_TIMESTAMP, 'RUNNING', 1, 100000, 4),
-                   ('application_1483347394756_2', 'query40', 1000, CURRENT_TIMESTAMP, 'RUNNING', 1, 1000000, 4),
-           ('application_1483347394756_3', 'query55', 1000, CURRENT_TIMESTAMP, 'RUNNING', 1, 100000, 4);
+    VALUES ('application_1483347394756_0', 'query26', 1000, '2017-01-01 00:00:00', 'RUNNING', 1, 200000, 4),
+                   ('application_1483347394756_1', 'query52', 1000, '2017-01-01 00:00:00', 'RUNNING', 1, 100000, 4),
+                   ('application_1483347394756_2', 'query40', 1000, '2017-01-01 00:00:00', 'RUNNING', 1, 1000000, 4),
+           ('application_1483347394756_3', 'query55', 1000, '2017-01-01 00:00:00', 'RUNNING', 1, 100000, 4);
 
 INSERT INTO `OPTIMIZER_CONFIGURATION_TABLE` (
   `application_id`,
