e7c1cf03eec90044e85e5c7fea0c51bdfd509f96
Marco Lattuada <marco.lattuada@polimi.it>
2017-11-17 11:31:13 +0100

    Fixed scripts/update_wsi_config.sh

--- scripts/update_wsi_config.sh
+++ scripts/update_wsi_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ -z ${DB_ADDRESS} ]; then
-    sed -i "s/<entry key=\"DB_IP\">.*<\/entry>/<entry key=\"DB_IP\">${DB_ADDRESS}<\/entry>/g;" ~/wsi_config.xml
+if [ ! -z ${DB_ADDRESS} ]; then
+    sed -i "s/<entry key=\"DB_IP\">.*<\/entry>/<entry key=\"DB_IP\">${DB_ADDRESS}<\/entry>/g;" /home/wsi/wsi_config.xml
 fi
-if [ -z ${DB_PORT} ]; then
-    sed -i "s/<entry key=\"DB_IP\">.*<\/entry>/<entry key=\"DB_IP\">${DB_PORT}<\/entry>/g;" ~/wsi_config.xml
+if [ ! -z ${DB_PORT} ]; then
+    sed -i "s/<entry key=\"DB_port\">.*<\/entry>/<entry key=\"DB_port\">${DB_PORT}<\/entry>/g;" /home/wsi/wsi_config.xml
 fi
