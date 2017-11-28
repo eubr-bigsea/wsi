7e317baffcad61e30afc1d33028cfda421aa5de2
Marco Lattuada <marco.lattuada@polimi.it>
2017-11-28 08:58:24 +0100

    Added bulma initial data

---
+++ Database/insertBulmaProfile.sql
@@ -0,0 +1,13 @@
+-- GENERATION DATABASE
+--     This script creates some stub data rows
+--
+-- Copyright 2017 <Biagio Festa>
+INSERT INTO `APPLICATION_PROFILE_TABLE` (
+  `application_id`,
+  `dataset_size`,
+  `phi_mem`,
+  `vir_mem`,
+  `phi_core`,
+  `vir_core`,
+  `chi_0`,
+  `chi_c`) VALUES ('bulma', 5, 4, 4, 2, 2, 599969.83, 4495992.00);
