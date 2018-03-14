b202fb5addc57a81dbd5336b5f6e09dc6463c77d
Marco Lattuada <marco.lattuada@polimi.it>
2018-03-14 16:02:17 +0100

    Added data about bulma_br

--- Database/insertBulmaData.sql
+++ Database/insertBulmaData.sql
@@ -1,7 +1,7 @@
 -- GENERATION DATABASE
 --     This script insert preliminary bulma results
 --
--- Copyright 2017 <Biagio Festa>
+-- Copyright 2017 <Marco Lattuad>
 
 INSERT INTO `OPTIMIZER_CONFIGURATION_TABLE` (
   `application_id`,
@@ -12,3 +12,16 @@ INSERT INTO `OPTIMIZER_CONFIGURATION_TABLE` (
    VALUES ('bulma', 5, 695000, 45, 23),
           ('bulma', 5, 912500, 23, 12),
           ('bulma', 5, 1130000, 16, 8);
+
+INSERT INTO `OPTIMIZER_CONFIGURATION_TABLE` (
+  `application_id`,
+  `dataset_size`,
+  `deadline`,
+  `num_cores_opt`,
+  `num_vm_opt`)
+   VALUES ('bulma_br', 5, 448000, 26, 13),
+          ('bulma_br', 5, 473000, 22, 8),
+          ('bulma_br', 5, 528484, 16, 8),
+          ('bulma_br', 5, 625000, 12, 6),
+          ('bulma_br', 5, 723000, 8, 4),
+          ('bulma_br', 5, 1213400, 4, 2);
--- Database/insertBulmaProfile.sql
+++ Database/insertBulmaProfile.sql
@@ -1,7 +1,7 @@
 -- GENERATION DATABASE
 --     This script creates some stub data rows
 --
--- Copyright 2017 <Biagio Festa>
+-- Copyright 2017 <Marco Lattuada>
 INSERT INTO `APPLICATION_PROFILE_TABLE` (
   `application_id`,
   `dataset_size`,
@@ -11,3 +11,12 @@ INSERT INTO `APPLICATION_PROFILE_TABLE` (
   `vir_core`,
   `chi_0`,
   `chi_c`) VALUES ('bulma', 5, 4, 4, 2, 2, 599969.83, 4495992.00);
+INSERT INTO `APPLICATION_PROFILE_TABLE` (
+  `application_id`,
+  `dataset_size`,
+  `phi_mem`,
+  `vir_mem`,
+  `phi_core`,
+  `vir_core`,
+  `chi_0`,
+  `chi_c`) VALUES ('bulma_br', 5, 4, 4, 2, 2, 212297.99, 3839230.86);
